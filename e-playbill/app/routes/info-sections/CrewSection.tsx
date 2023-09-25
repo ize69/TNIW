b34978b953d0e2a6e30a696e7e0df6e110155d8f
isaac worsencroft <47464523+ize69@users.noreply.github.com>
2023-09-25 06:39:27 -0400

    info pages bios for crew and cast

--- e-playbill/app/routes/info-sections/CrewSection.tsx
+++ e-playbill/app/routes/info-sections/CrewSection.tsx
@@ -1,21 +1,29 @@
-// CrewSection.tsx
-// defines the cast section of the info page
 import React from "react";
 import type { LinksFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
 import { Outlet } from "react-router-dom";
 import stylesUrl from "~/styles/info-sections.css";
-export const links: LinksFunction = () => [{ rel: "stylesheet", href: stylesUrl },];
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: stylesUrl }];
+import { peopleData } from "./data"; // Import the JSON data
 
 function CrewSection() {
+  const roleToCheck = "crew";
+  const filteredData = peopleData.filter((person) => person.crew === roleToCheck);
 
-    return (
-        <div>
-        <h2>Crew</h2>
-        <p>Crew info</p>
-        </div>
-    );
-    }
+  return (
+    <div>
+      <h2>Crew</h2>
+      <p>Crew info</p>
+      <div className="crew-members">
+        {filteredData.map((person) => (
+          <div key={person.first} className="crew-member">
+            <img src={`assets/${person.photoPath}`} alt={`${person.first} ${person.last}`} className="headshots"/>
+            <p>{`${person.first} ${person.last}`}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default CrewSection;
-
---
+++ e-playbill/app/routes/info-sections/data.js
@@ -0,0 +1,21 @@
+// data.js
+export const peopleData = [
+    {
+      first: "Joe",
+      last: "Smoe",
+      crew: "cast",
+      billingPriority: 1,
+      photoPath: "headshots/headshot-filler.png",
+      whatTheyDo: "stuff",
+    },
+    {
+      first: "John",
+      last: "Smith",
+      crew: "crew",
+      billingPriority: 1,
+      photoPath: "headshots/headshot-filler.png",
+      whatTheyDo: "acting",
+    },
+    // Add more data here
+  ];
+  
