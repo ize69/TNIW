54461ba1f8f04bafac7e35159ce3d31db63e30a4
isaac worsencroft <47464523+ize69@users.noreply.github.com>
2023-10-26 01:13:40 -0400

    Update Info.tsx

--- e-playbill/app/routes/Info.tsx
+++ e-playbill/app/routes/Info.tsx
@@ -6,23 +6,29 @@ import { Link } from "@remix-run/react";
 import { Outlet } from "react-router-dom";
 import stylesUrl from "~/styles/index.css";
 export const links: LinksFunction = () => [
-    { rel: "stylesheet", href: stylesUrl },
-  ];
-  
+  { rel: "stylesheet", href: stylesUrl },
+];
+
 function Info() {
   //renders the info page
   //rerenders the info page when the role changes
   console.log("Info component re-rendered");
-  const [role, setRole] = React.useState("crew");
+  const [role, setRole] = React.useState("cast");
+  const [key, setKey] = React.useState(0);
+
+  const handleRoleChange = (role: string) => {
+    setKey(key + 1);
+    setRole(role);
+  };
+
   return (
-    <div>
+    <div key={key}>
       <TopSection /> {/* Use the TopSection component */}
       {/* Additional content specific to the Info page */}
       {/*<CastSection />*/}
       {/*<CrewSection />*/}
       {/*<PeopleFilter />*/}
-      <PeopleFilter role={role} setRole={setRole} />
-      
+      <PeopleFilter role={role} setRole={handleRoleChange} />
     </div>
   );
 }
