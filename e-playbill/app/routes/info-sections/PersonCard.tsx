4ec8971f43b3960e087b633c8b7ba9faa7000c41
isaac worsencroft <47464523+ize69@users.noreply.github.com>
2023-11-05 18:25:53 -0500

    add fade to images

--- e-playbill/app/routes/info-sections/PersonCard.tsx
+++ e-playbill/app/routes/info-sections/PersonCard.tsx
@@ -18,11 +18,12 @@ const PersonCard: React.FC<PersonCardProps> = ({ person }) => {
   return (
     <div key={person.role} className='castMember'>
       <div className="person-info">
-        
         <span className="name">{`${person.first} ${person.last}`}</span>
         <span className="role">as {person.role}</span>
       </div>
-      <img src={`assets/${person.photoPath}`} alt={`${person.first} ${person.last}`} className="headshots" />
+      <div className="image-wrapper">
+        <img src={`assets/${person.photoPath}`} alt={`${person.first} ${person.last}`} className="headshots" />
+      </div>
       <p className="bio">{person.bio}</p>
     </div>
   );
