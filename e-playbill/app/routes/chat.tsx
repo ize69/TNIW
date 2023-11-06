b848ad367d6a32bb61a0118839208177e4164945
isaac worsencroft <47464523+ize69@users.noreply.github.com>
2023-11-05 23:29:52 -0500

    Update chat.tsx

--- e-playbill/app/routes/chat.tsx
+++ e-playbill/app/routes/chat.tsx
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import TopSection from "./TopSection"; // Import the TopSection component
 import type { LinksFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
-import { Outlet } from "react-router-dom";
 import stylesUrl from "~/styles/index.css";
 
 export const links: LinksFunction = () => [
@@ -11,26 +9,39 @@ export const links: LinksFunction = () => [
 
 export default function Chat() {
   useEffect(() => {
-    const script1 = document.createElement('script');
-    script1.textContent = `
-      var remark_config = {
-        host: '172.0.0.1',
-        site_id: 'remark',
-      };
-    `;
-    document.body.appendChild(script1);
+    const loadScripts = async () => {
+      try {
+        const remarkConfigScript = document.createElement("script");
+        remarkConfigScript.textContent = `
+          var remark_config = {
+            host: '172.0.0.1',
+            site_id: 'remark',
+          };
+        `;
+        document.body.appendChild(remarkConfigScript);
 
-    const script2 = document.createElement('script');
-    script2.textContent = `
-      !function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);
-    `;
-    document.body.appendChild(script2);
+        const remarkScripts = document.createElement("script");
+        remarkScripts.textContent = `
+          !function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);
+        `;
+        document.body.appendChild(remarkScripts);
 
+        return () => {
+          document.body.removeChild(remarkConfigScript);
+          document.body.removeChild(remarkScripts);
+        };
+      } catch (error) {
+        console.error("Error loading scripts:", error);
+      }
+    };
+
+    loadScripts();
+
+    // Clean up function will be called on component unmount
     return () => {
-      document.body.removeChild(script1);
-      document.body.removeChild(script2);
-    }
-  }, []);
+      // Clean up logic (if any) here
+    };
+  }, []); // Empty dependency array ensures the effect runs once after the initial render
 
   return (
     <div>
