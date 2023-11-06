7624684c703038bf9d7f60f9210d06dba5026a6e
isaac worsencroft <47464523+ize69@users.noreply.github.com>
2023-11-05 23:21:01 -0500

    Update chat.tsx

--- e-playbill/app/routes/chat.tsx
+++ e-playbill/app/routes/chat.tsx
@@ -10,52 +10,32 @@ export const links: LinksFunction = () => [
 ];
 
 export default function Chat() {
-  const [serverFound, setServerFound] = useState(true); // Initially assume the server is found
-
   useEffect(() => {
-    //TODO: make a request to the remark42 server to see if it is accessible once a remark42 server is available
-    // Perform a check to see if the server is accessible
-    // You can make an HTTP request or perform any necessary check here
-    // If the server is not found, setServerFound(false);
-    // Example:
-    // fetch("https://your-remark42-server-url")
-    //   .then((response) => {
-    //     if (!response.ok) {
-    //       setServerFound(false);
-    //     }
-    //   })
-    //   .catch(() => {
-    //     setServerFound(false);
-    //   });
+    const script1 = document.createElement('script');
+    script1.textContent = `
+      var remark_config = {
+        host: '172.0.0.1',
+        site_id: 'remark',
+      };
+    `;
+    document.body.appendChild(script1);
+
+    const script2 = document.createElement('script');
+    script2.textContent = `
+      !function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);
+    `;
+    document.body.appendChild(script2);
+
+    return () => {
+      document.body.removeChild(script1);
+      document.body.removeChild(script2);
+    }
   }, []);
 
   return (
     <div>
-      <script>
-        {`
-          var remark_config = {
-            host: '172.0.0.1',
-            site_id: 'remark',
-          };
-          
-        `}
-      </script>
-      <script>
-        {'!function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);'}
-      </script>
-      <div id="remark42"></div>
       <TopSection /> {/* Use the TopSection component */}
-      {serverFound ? (
-        // Render your chat content when the server is found
-        <div>
-          {/* Additional content specific to the Chat page that will call the remark42 microblogging server */}
-        </div>
-      ) : (
-        // Render an error message when the server is not found
-        <div>
-          <p className="error">Error: The server is not accessible.</p>
-        </div>
-      )}
+      <div id="remark42"></div>
     </div>
   );
 }
